df1afbdaf536c9864e4e81eced0b4e5e6bf213fd
leem48 <52877974+leem48@users.noreply.github.com>
2019-07-14 15:28:10 +0300

    Add files via upload

--- PciResurces.py
+++ PciResurces.py
@@ -8,7 +8,7 @@ class PciResurces(feature.feature):
     def __init__(self):
         self.CR_space_agent = None
         self.conf_space_agent = None
-        self.cli_agent = None
+
 
     def print_feature(self):
         print "This is the CR space:{}\n".format(self.CR_space_agent.get_CRspace())
@@ -32,16 +32,9 @@ class PciResurces(feature.feature):
     def get_Confspace_agent(self):
         return self.conf_space_agent
 
-    def get_Cli_agent(self):
-        return self.cli_agent
-
     def set_CRspace_agent(self, cr_space):
         self.CR_space_agent = cr_space
 
     def set_Confspace_agent(self, conf_space):
         self.conf_space_agent = conf_space
-
-    def set_Cli_agent(self, cli):
-        self.cli_agent = cli
-
 
