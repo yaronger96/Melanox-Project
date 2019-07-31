8e9ee8a0e10074e6a531dae0a7c803ee5f7baf64
yaronger96 <50360081+yaronger96@users.noreply.github.com>
2019-07-31 11:56:40 +0300

    Add files via upload

---
+++ PcieCheckErrors.py
@@ -0,0 +1,32 @@
+import PcieTest
+
+
+class PcieCheckErrors(PcieTest.PcieTest):
+    def __init__(self):
+        PcieTest.PcieTest.__init__(self)
+        self.l_args = None
+
+    def addArgs(self):
+        parser = PcieTest.argparse.ArgumentParser()
+        parser.add_argument("--speed", help="Expected speed in gens(1,2,3,4..)",default=None, type=int)
+        parser.add_argument("--width", help="Expected width (1,2,4,8,16..)", default=None, type=int)
+        self.l_args, unknown = parser.parse_known_args()
+        if self.l_args.speed is None:
+            print "Please state expected speed, exiting.."
+            exit(1)
+        if self.l_args.width is None:
+            print "Please state expected width, exiting.."
+            exit(1)
+
+    def preTest(self):
+        print "check errors"
+
+    def postTest(self):
+        print "check errors"
+
+    def run(self):
+        print "check errors"
+
+if __name__=='__main__':
+    test = PcieCheckErrors()
+    test.main()
---
+++ PcieTest.py
@@ -0,0 +1,46 @@
+import argparse
+from abc import abstractmethod
+import sys
+sys.path.insert(0, '/auto/sw_work/fwshared/c_yarong/pci_verification/student_project')
+import Monostate.Monostate as Monostate
+class PcieTest:
+
+    def __init__(self):
+        self.g_args = None
+
+    def parseArgs(self):
+        parser = argparse.ArgumentParser()
+        #parser.add_argument("testName", help="The name of the desired test")
+        parser.add_argument("dut", help="One side of the link that we want to test:BDF,CR,MTUSB")
+        parser.add_argument("--serverName", type=str, help="Insert IP if you want to work on a remote server",
+                            default="Local")
+        #parser.add_argument("-i", help="number of iterations", default=1)
+        parser.add_argument("-p", help="Port number, Insert when using cr space device",type=int)
+        self.g_args, unknown = parser.parse_known_args()
+        print "parseArgs"
+
+
+    @abstractmethod
+    def run(self):
+        pass
+
+    @abstractmethod
+    def preTest(self):
+        pass
+
+    @abstractmethod
+    def postTest(self):
+        pass
+
+    @abstractmethod
+    def addArgs(self):
+        pass
+
+    def main(self):
+        self.parseArgs()
+        self.addArgs()
+        server = Monostate.Monostate(self.g_args.dut, self.g_args.serverName, self.g_args.p)
+        self.preTest()
+        self.run()
+        self.postTest()
+
