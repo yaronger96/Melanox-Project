fa253e2c59b45e6b45da2957c92277e17566ad8b
yaronger96 <50360081+yaronger96@users.noreply.github.com>
2019-08-01 12:02:12 +0300

    Add files via upload

--- EventHendler.py
+++ EventHendler.py
@@ -1,4 +1,4 @@
-
+from prettytable import PrettyTable
 class EventHendler:
     def __init__(self):
         self.EventHendler_dict = dict()
@@ -6,6 +6,15 @@ class EventHendler:
 
     def addEvent(self,iter,error,bdf,uscOrDsc):
         if iter in self.EventHendler_dict.keys():
-            self.EventHendler_dict[iter].append([error, device, uscOrDsc])
-        self.EventHendler_dict[iter] = [error, device, uscOrDsc]
+            self.EventHendler_dict[iter].append([error, bdf, uscOrDsc])
+        self.EventHendler_dict[iter] = [error, bdf, uscOrDsc]
 
+    def printEventHandler(self):
+        SetupTtable = PrettyTable()
+        SetupTtable.field_names = ["Iteration", "Error", "USC/DSC", "BDF"]
+        for iteration in self.EventHendler_dict.keys():
+            list = self.EventHendler_dict[iteration]
+            error = list[0]
+            bdf = list[1]
+            uscOrDsc = list[2]
+            SetupTtable.add_row([str(iteration), str(error), str(bdf), str(uscOrDsc)])
